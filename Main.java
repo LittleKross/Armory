f659f0e3edd8c9c44c693d117453bcfea5c32d65
Brandon Young <btyoung2k@gmail.com>
2020-02-06 15:06:30 -0500

    Updating the ramdomArmorGenerator function to accept int parameter to specify # of items to be generated

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     RandomArmorGenerator.randomArmorGenerator();
+    System.out.println("========================================================================================");
+    RandomArmorGenerator.randomArmorGenerator(10);
   }
 }
