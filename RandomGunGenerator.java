8d239df773b7a477b4beebe72c8d74daf724f87e
Brandon Young <btyoung2k@gmail.com>
2020-02-07 11:53:08 -0500

    Updated RandomGunGenerator.java to accept int param. Updated RandomMeleeGenerator.java to accept int param.

--- RandomGunGenerator.java
+++ RandomGunGenerator.java
@@ -1,23 +1,35 @@
 import java.util.Random;
 
 public class RandomGunGenerator {
+
+  private static Random rand = new Random();
+  private static String[] brands = {"Remington", "Battle Rack", "Galatrax", "Inferno", "Uronia", "Manacle", "Carbor", "Glea", "Shackle", "Dalarax", "Gorog"};
+  private static String[] models = {"Hogbuster", "Shiny Shooter", "Bolt Breaker", "Greedy", "Sheetcutter", "Hullbuster", "Hellfire", "Firebreather", "Tank Blaster"};
+  private static String[] versions = {"mkI", "mkII", "mkIII", "mkIV", "mkV", "v1", "v2", "v3", "v4", "v5", "prototype"};
+  private static String[] types = {"Auto Rifle", "Pulse Rifle", "Scout Rifle", "Auto Pistol", "Pulse Pistol", "Scout Pistol", "Flamethrower", "Rocket Launcher", "Sniper Rifle", "Focus Rifle", "Machine Gun"};
+  private static int[] damagePerUse = {90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300};
+  private static int[] roundCapacity = {1, 2, 10, 15, 20, 25, 30, 35, 40, 45, 50};
   
   public static void randomGunGenerator() {
     
     Gun[] guns = new Gun[1];
-    Random rand = new Random();
-    String[] brands = {"Remington", "Battle Rack", "Galatrax", "Inferno", "Uronia", "Manacle", "Carbor", "Glea", "Shackle", "Dalarax", "Gorog"};
-    String[] models = {"Hogbuster", "Shiny Shooter", "Bolt Breaker", "Greedy", "Sheetcutter", "Hullbuster", "Hellfire", "Firebreather", "Tank Blaster"};
-    String[] versions = {"mkI", "mkII", "mkIII", "mkIV", "mkV", "v1", "v2", "v3", "v4", "v5", "prototype"};
-    String[] types = {"Auto Rifle", "Pulse Rifle", "Scout Rifle", "Auto Pistol", "Pulse Pistol", "Scout Pistol", "Flamethrower", "Rocket Launcher", "Sniper Rifle", "Focus Rifle", "Machine Gun"};
-    int[] damagePerUse = {90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300};
-    int[] roundCapacity = {1, 2, 10, 15, 20, 25, 30, 35, 40, 45, 50};
     
     for(int i = 0; i < guns.length; i++) {
       guns[i] = new Gun(brands[rand.nextInt(brands.length)], models[rand.nextInt(models.length)], versions[rand.nextInt(versions.length)], types[rand.nextInt(types.length)], i + 100, damagePerUse[rand.nextInt(damagePerUse.length)], roundCapacity[rand.nextInt(roundCapacity.length)]);
       System.out.println(guns[i].gunDescribe());
     }
   }
+
+  public static void randomGunGenerator(int n) {
+    
+    Gun[] guns = new Gun[n];
+    
+    for(int i = 0; i < guns.length; i++) {
+      guns[i] = new Gun(brands[rand.nextInt(brands.length)], models[rand.nextInt(models.length)], versions[rand.nextInt(versions.length)], types[rand.nextInt(types.length)], i + 100, damagePerUse[rand.nextInt(damagePerUse.length)], roundCapacity[rand.nextInt(roundCapacity.length)]);
+      System.out.println(guns[i].gunDescribe());
+    }
+  }
+
   public static void main(String[] args) {
    randomGunGenerator();
   }
--- RandomMeleeGenerator.java
+++ RandomMeleeGenerator.java
@@ -2,21 +2,33 @@ import java.util.Random;
 
 public class RandomMeleeGenerator {
   
+  private static Random rand = new Random();
+  private static String[] brands = {"Manacle", "Battle Rack", "Mandarin Arms", "Corosive", "Vibro", "Clan", "Reviler", "Cleaver"};
+  private static String[] models = {"Craken", "Cyclops", "Zeus", "Hades", "Gargoyle"};
+  private static String[] versions = {" mkI", " mkII", " mkIII", " mkIV", " mkV", ".v1", ".v2", ".v3", ".v4", ".v5", " prototype"};
+  private static String[] types = {"Knife", "Short Sword", "Long Sword", "Battleaxe", "Warhammer", "Vibroblade", "Machette", "Electro-Whip", "Electro-Blade"};
+  private static int[] damagePerUse = {90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300};
+
   public static void randomMeleeGenerator() {
     
     MeleeWeapon[] meleeWeapons = new MeleeWeapon[1];
-    Random rand = new Random();
-    String[] brands = {"Manacle", "Battle Rack", "Mandarin Arms", "Corosive", "Vibro", "Clan", "Reviler", "Cleaver"};
-    String[] models = {"Craken", "Cyclops", "Zeus", "Hades", "Gargoyle"};
-    String[] versions = {" mkI", " mkII", " mkIII", " mkIV", " mkV", ".v1", ".v2", ".v3", ".v4", ".v5", " prototype"};
-    String[] types = {"Knife", "Short Sword", "Long Sword", "Battleaxe", "Warhammer", "Vibroblade", "Machette", "Electro-Whip", "Electro-Blade"};
-    int[] damagePerUse = {90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300};
     
     for(int i = 0; i < meleeWeapons.length; i++) {
       meleeWeapons[i] = new MeleeWeapon(brands[rand.nextInt(brands.length)], models[rand.nextInt(models.length)], versions[rand.nextInt(versions.length)], types[rand.nextInt(types.length)], i + 100, damagePerUse[rand.nextInt(damagePerUse.length)]);
       System.out.println(meleeWeapons[i].meleeDescribe());
     }
   }
+
+  public static void randomMeleeGenerator(int n) {
+    
+    MeleeWeapon[] meleeWeapons = new MeleeWeapon[n];
+    
+    for(int i = 0; i < meleeWeapons.length; i++) {
+      meleeWeapons[i] = new MeleeWeapon(brands[rand.nextInt(brands.length)], models[rand.nextInt(models.length)], versions[rand.nextInt(versions.length)], types[rand.nextInt(types.length)], i + 100, damagePerUse[rand.nextInt(damagePerUse.length)]);
+      System.out.println(meleeWeapons[i].meleeDescribe());
+    }
+  }
+
   public static void main(String[] args) {
    randomMeleeGenerator();
   }
